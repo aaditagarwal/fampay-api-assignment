5f43a5dd85033ff5b0283135fdf55516101ed7f8
Aadit Agarwal <agarwal.aadit99@gmail.com>
2022-06-27 14:24:54 +0530

    Video cards Added

---
+++ frontend/src/components/VideoCard.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import withStyles from 'react-jss';
+import { styles } from './Videocard.styles';
+
+interface VideoCardType{
+    classes: {
+        [X in keyof (typeof styles)]: string;
+    };
+    title: string;
+    description: string;
+    thumbnail_default_res: string;
+    thumbnail_high_res: string;
+};
+
+const Videocard = (props: VideoCardType) => {
+
+    const { classes, title, description, thumbnail_default_res, thumbnail_high_res } = props;
+
+    return(
+        <div className={classes.card}>
+            
+            <div className={classes.thumbnail}>
+                <img alt={title} src={thumbnail_high_res}/>
+            </div>
+
+            <div className={classes.content}>
+                <h3 className={classes.title}>{title}</h3>
+                <p>{description}</p>
+            </div>
+
+        </div>
+    );
+};
+
+export default withStyles(styles)(Videocard);
---
+++ frontend/src/components/Videocard.styles.tsx
@@ -0,0 +1,27 @@
+export const styles = {
+    card: {
+        color: "aliceblue",
+        margin: "20px 20px 0 0",
+        border: "5px solid #333",
+        overflow: "auto",
+    },
+
+    thumbnail: {
+        float: "left",
+        display: "block"
+    },
+
+    content: {
+        paddingLeft: "10%",
+        paddingRight: "5px"
+    },
+
+    title: {
+        marginTop: "10%",
+        paddingLeft: "10%",
+        paddingRight: "5px",
+        fontWeight: "bold",
+        fontSize: "200%",
+    },
+
+};
